444726d8d14e4a2c71e72b023f6d9ce75245f843
arshil692-prog <muktars692@gmail.com>
2026-07-31 17:34:31 +0530

    slove 17 ex

---
+++ DATA_TYPES.py
@@ -0,0 +1,160 @@
+# 1///
+# DATA TYPE TELLS TO PYTHON WHAT KIND OF VALUE STORE 
+
+# 2///
+"""
+1. STRING
+2. INTGER
+3. FLOAT
+4. FLOAT
+5. BOOLEAN
+6. SET 
+7. DICTINARY
+8. LIST
+9. TUPLE
+"""
+
+# 3///
+name = "arshil"
+age = 19
+marks = 89.90
+good_student = True
+compelx = 12j
+
+print(name)
+print(age)
+print(good_student)
+print(compelx)
+print(marks)
+
+# 4///
+a = 25 
+print(type(a))
+
+# 5///
+a = 25j
+print(type(a))
+
+# 6///
+a = "name"
+print(type(a))
+
+# 7///
+a = True
+print(type(a))
+
+# 8////
+a = 3+4j
+print(type(a))
+
+# 9///
+student_age = 22
+student_marks = 88.00
+student_think = 12j
+
+print(student_age)
+print(student_marks)
+print(student_think)
+
+# 10////
+age = int(input("enter your num"))
+print(type(age))
+
+# 11////
+marks = float(input("enter your marks"))
+print(type(marks))
+
+# 12////
+empolyee_name = "arshil shaikh"
+empolyee_salery =  20,0000 
+empolyee_imajin = 12j
+empolyee_is_adult = True
+empolyee_post = ["maneger","HR","devloper"]
+
+print(empolyee_imajin)
+print(empolyee_is_adult)
+print(empolyee_post)
+print(empolyee_salery)
+print(empolyee_name)
+
+# 13///
+"""
+num = 20
+print(type num)
+
+find the error
+
+answer > invalid synatx
+"""
+num = 20
+print(type(num))
+
+# 14/////
+"""
+Which of the following is a Boolean value?
+
+A. "True"
+
+B. True
+
+C. TRUE
+
+D. true
+
+answer >  A. True
+
+"""
+# 15////
+
+#Read the code and identify the data type of each variable.
+
+#a = 50
+#b = 5.5
+#c = "Hello"
+#d = False
+#e = 5+2j
+
+"""
+answer > 
+int
+float
+string
+boolean
+complex
+"""
+# 16////
+
+name = "arshil"
+numbers = 12
+compellx = 12j 
+marks = 99.99
+good = True 
+l1 = [1,2,3]
+s1 = {11,33,54,33}
+student_info = {
+    "name":"arshil shaikh",
+    "age" : 19,
+    "address":"niphad"
+}
+t1 = ("hello","hii")
+
+print(name)
+print(numbers)
+print(compellx)
+print(marks)
+print(s1)
+print(l1)
+print(t1)
+print(student_info)
+
+# 17////
+"""   
+                   Feature	Mutable Data Types	               Immutable Data Types
+Meaning:	    Can be changed after creation.      	Cannot be changed after creation.
+Memory:	        Same object is modified.	            A new object is created when modified.
+Modification:	Allowed.	                            Not allowed.
+Performance:    Faster for frequent updates.	        Safer because values cannot change accidentally.
+Examples:	    list, dict, set, bytearray	            int, float, bool, str, tuple, frozenset,betys
+"""
+
+# 18////
