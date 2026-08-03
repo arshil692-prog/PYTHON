06a348cc293b0b45bc5dd8c2286eb508dc6d1726
arshil692-prog <muktars692@gmail.com>
2026-08-03 13:04:17 +0530

    example 03/08

---
+++ OPERATORS.py
@@ -0,0 +1,80 @@
+"""
+# 1///
+# ANSWER > : oprators are special symbol to perform oprations on(on oneo two operands) variables and therir values. 
+
+#2///
+# ANSWER > : there are 7 types of operators in python 
+# 1. arithamatic operators
+# 2. assignment operators 
+# 3. comparison operators 
+# 4. logical operators 
+# 5. identity operators 
+# 6. membership operators
+# 7. bitwise opertors
+
+# 3///
+a = 10 
+b = 20 
+print(a+b)
+
+# 4////
+a = 10 
+b = 5
+print(a-b)
+
+# 5////
+a = 5 
+b = 3
+print(a*b)
+
+# 6///
+a = 10 
+b = 3 
+print(a/b)
+
+#7////
+a = 10
+b = 3
+print(a%b)
+
+# 8////
+a = 10
+b = 1
+print(a//b)
+
+# 9///
+base = float(input("Enter the base:"))
+exponent = float(input("Enter the exponent:"))
+result = base**exponent
+print("result:", (result))
+
+# 10///
+print(10 + 5 * 2)
+# ANSWER > : 20 
+
+# 11////
+print(20 // 3)
+# ANSWER > : 6
+
+# 12///
+print(20 % 3)
+# ANSWER > : 2
+
+# 13//
+num = int(input("Enter a num:"))
+squre = num ** 2 
+print("squre=",squre)
+
+# 14////
+num1 = int(input("Enter num1:"))
+num2 = int(input("Enter num2:"))
+
+print("additions:",num1 + num2)
+print("subtraction:",num1 - num2)
+print("multiplication:",num1 * num2)
+print("division:",num1 / num2)
+print("modulus:",num1 % num2)
+print("floor division:",num1 // num2)
+print("exponent:",num1 ** num2)
+"""
+# 15////
