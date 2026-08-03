ac1aca74f61bd0a593ad28aa897d747cf7a50e1c
arshil692-prog <muktars692@gmail.com>
2026-08-03 16:12:01 +0530

    ex 03/08

--- OPERATORS.py
+++ OPERATORS.py
@@ -76,5 +76,95 @@ print("division:",num1 / num2)
 print("modulus:",num1 % num2)
 print("floor division:",num1 // num2)
 print("exponent:",num1 ** num2)
-"""
+
 # 15////
+a = "20"
+b = 5
+#print(a - b)
+# answer>:
+print(int(a))
+print(int(a)-b)
+
+# 16////
+a = 10
+b = 20
+print(a / b)
+# ANSWER > the operator is performs the normal division and returns the float value.
+
+a = 10
+b = 20
+print(a // b) 
+# ANSWER > the operator is performs the floor division and returns the integer value.
+
+# 17//////
+= (Assignment Operator)
+The = operator is used to assign (store) a value in a variable. 
+
+==(Comparison Operator)
+The == operator is used to compare two values. It returns True if the values are equal, otherwise it returns False.
+
+# 18/////
+num = int(input("Enter a number:"))
+if num % 2 == 0:
+    print(num,"is even")
+else:
+    print(num,"is odd")
+
+# 19////
+num = int(input("Enter a number:"))
+if num % 5 == 0 and num % 7 == 0:
+    print(num,"is divisible by both 5 and 7")
+else:
+    print(num,"is not divisible by both 5 and 7")
+
+# 20////
+num = int(input("Enter a number:"))
+last_digit = num % 10
+print("Last digit:",last_digit)
+
+# 21////
+a = 10
+b = 22
+print("a =", b, "b =", a)
+
+a = 20
+b = 10
+a = a + b
+b = a - b
+a = a - b
+print(a)
+print(b)
+
+# 22///
+num = int(input("Enter the number:"))
+cube = num **3
+print("cube=",cube)
+
+# 23///
+print(5 > 2)
+
+# 24///
+print(5 != 5)
+
+# 24///
+print(5 <= 5)
+
+# 25///
+print(5 <= 5)
+
+# 26///
+#(==)Equal
+#(!=)NOt equal
+#(>)grether then
+#(<)less then
+#(>=)grether then equal
+#(<=)less then equal
+
+print("equal:,",10==10)
+print("Not equal:",10 != 3)
+print("greter then:",10>1)
+print("less then:",5<10)
+print("greter then equal:",11>=10)
+print("less then equal:",1<=33)
+"""
+# 27///
