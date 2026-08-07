797b76fa0403dff8e630b8b519f538fc9c44d8aa
arshil692-prog <muktars692@gmail.com>
2026-08-07 16:38:09 +0530

    PRACTICE ON DATA TYPES

---
+++ CONDITINAL_STATEMENT.py
@@ -0,0 +1,20 @@
+# 1///
+# What are Conditional Statements in Python? Explain in your own words.
+
+# A Conditional Statement is a decision-making statement in Python that allows a,
+#  program to execute different blocks of code based on whether a specified condition is True or False.
+
+# 2///
+
+# Imagine you want to withdraw ₹5,000 from an ATM.
+
+# The ATM first checks:
+
+# Condition: Is your account balance greater than or equal to ₹5,000?
+
+# If Yes → The ATM dispenses the money.
+# If No → The ATM displays "Insufficient Balance" and does not dispense cash.
+
+# This is exactly how a conditional statement works—it checks a condition first and then decides which action to perform.
+
+# 3////
